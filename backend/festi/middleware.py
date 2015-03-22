ed39bc5e821d1688af8164780f58e6cd74bf34e1
Chinseok Lee <allieuslee@gmail.com>
2015-03-22 17:33:37 +0900

    json response middleware 에 set 타입 추가

--- backend/festi/middleware.py
+++ backend/festi/middleware.py
@@ -1,6 +1,4 @@
 import json
-from hashlib import md5
-from django.conf import settings
 from django.db.models import Model
 from django.db.models.query import QuerySet
 from django.http import HttpResponse, QueryDict
@@ -21,7 +19,7 @@ class JsonResponseMiddleware(object):
             if response and response[0] in ('"', '[', '{'):
                 return HttpResponse(response, content_type='application/json')
             return HttpResponse(response)
-        elif isinstance(response, (dict, list, tuple, QuerySet, Model)):
+        elif isinstance(response, (set, dict, list, tuple, QuerySet, Model)):
             json_string = json.dumps(response, cls=DjangoJSONEncoder, ensure_ascii=False)
             return HttpResponse(json_string, content_type='application/json')
         else:
